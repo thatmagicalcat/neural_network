d1c7009d60d85866a2455f0268b4279aa40fea32
Pranjal-Patel <pranjalpatel9838@gmail.com>
2023-11-26 12:47:28 +0530

    implemented xor operator predictor

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod matrix;
+pub mod activations;
+pub mod network;
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+// A neural network that can predict the output of the xor operator
+
+use neural_network::activations;
+use neural_network::network::Network;
+
+fn main() {
+    // Data
+    let inputs = vec![vec![0., 0.], vec![1., 0.], vec![0., 1.], vec![1., 1.]];
+    let outputs = vec![vec![0.0], vec![1.0], vec![1.0], vec![0.0]];
+
+    let mut network = Network::new(
+        vec![2, 3, 1],
+        0.6,
+        activations::SIGMOID,
+    );
+
+    println!("Before training");
+    println!("0, 0 = {:?}", network.feed_forward(vec![0., 0.])[0]);
+    println!("1, 0 = {:?}", network.feed_forward(vec![1., 0.])[0]);
+    println!("0, 1 = {:?}", network.feed_forward(vec![0., 1.])[0]);
+    println!("1, 1 = {:?}", network.feed_forward(vec![1., 1.])[0]);
+
+    // train
+    network.train(inputs, outputs, 10000);
+
+    println!("After training");
+    println!("0, 0 = {:?}", network.feed_forward(vec![0., 0.])[0]);
+    println!("1, 0 = {:?}", network.feed_forward(vec![1., 0.])[0]);
+    println!("0, 1 = {:?}", network.feed_forward(vec![0., 1.])[0]);
+    println!("1, 1 = {:?}", network.feed_forward(vec![1., 1.])[0]);
+}
