9c80655dbc9720163e21bf0c0ee5ac5ed52891ae
Pranjal-Patel <pranjalpatel9838@gmail.com>
2023-11-26 17:37:45 +0530

    added more activation functions

--- src/activations.rs
+++ src/activations.rs
@@ -10,3 +10,13 @@ pub const SIGMOID: Activation = Activation {
     function: &|x| 1.0 / (1.0 + E.powf(-x)),
     derivative: &|x| x * (1.0 - x),
 };
+
+pub const TANH: Activation = Activation {
+    function: &|x| x.tanh(),
+    derivative: &|x| 1.0 - x.tanh().powf(x),
+};
+
+pub const RELU: Activation = Activation {
+    function: &|x| 0.0f64.max(x),
+    derivative: &|x| if x > 0.0 { 1.0 } else { 0.0 },
+};
